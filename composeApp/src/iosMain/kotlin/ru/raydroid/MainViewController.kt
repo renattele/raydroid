5b53d5aa12206fb674f840f6294183be2a0d34fe
renattele <renattele20@gmail.com>
2026-02-21 23:52:44 +0300

    init

---
+++ composeApp/src/iosMain/kotlin/ru/raydroid/MainViewController.kt
@@ -0,0 +1,5 @@
+package ru.raydroid
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ shared/src/commonMain/kotlin/ru/raydroid/Constants.kt
@@ -0,0 +1,3 @@
+package ru.raydroid
+
+const val SERVER_PORT = 8080
